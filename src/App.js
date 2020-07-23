3211211bc0593de39e23e7efdc83c5b378f983a5
rafik gasmi <gasmi.rafik.rg@gmail.com>
2020-07-23 17:52:52 +0100

    checkpoint jsx

--- src/App.js
+++ src/App.js
@@ -2,23 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div style={{border:'solid 1px black',maxWidth:'100vw'}}>  
+        
+        
+        <h1 className="title red">RAFIK GASMI</h1>
+          <br/> 
+        <img src={"https://effetsdelamusiquesurlhomme.files.wordpress.com/2016/02/image1.jpg?w=1000"} /> <br/>
+        <img src={"https://img.pixers.pics/pho_wat(s3:700/FO/69/92/06/09/700_FO69920609_eef37e4a3570c5a7771e81028417c75c.jpg,700,700,cms:2018/10/5bd1b6b8d04b8_220x50-watermark.png,over,480,650,jpg)/papiers-peints-music-equalizer-wave.jpg.jpg"} /> 
+        </div>
+        <div className="video" >
+
+         <iframe src="https://www.youtube.com/embed/ERhliMqaZ9w" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+     </div>
+      
     </div>
   );
 }
